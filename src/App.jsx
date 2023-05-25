b4dfcde5f545bb396809ccdae637ca10af58ecdb
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-05-25 11:20:15 -0300

    Project Start: Header and Sidebar built

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Post } from './Post';
+import { Header } from './components/Header';
+import './global.css'
+import styles from './App.module.css';
+import { Sidebar } from './components/Sidebar';
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post author="Diego" content="Lorem Ipsum" />
+          <Post author="Gabriel" content="Um novo post legal" />
+        </main>
+      </div>
+    </div>
+  )
+}
