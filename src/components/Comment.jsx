be6470f9372f25b10613cf8b1c9bdbd08d3e685b
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-05-26 10:47:34 -0300

    Created Comment Session for the Posts

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,31 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 
 export const Comment = () => {
   return (
-    <div>Comment</div>
+    <div className={styles.comment}>
+        <img src="https://github.com/kunzeleric.png" alt=""/>
+        <div className={styles.commentBox}>
+            <div className={styles.commentContent}>
+                <header>
+                    <div className={styles.authorAndTime}>
+                        <strong>Eric Kunzel</strong>
+                        <time title="26 de Maio às 9:03h" dateTime="2023-05-26 9:03:00">Cerca de 2h atrás</time>
+                    </div>
+                    <button title="Deletar comentário">
+                        <Trash size={24} />
+                    </button>
+                </header>
+                <p>Muito bom Devon, parabéns!! 👏👏</p>
+            </div>
+        
+        <footer>
+            <button>
+                <ThumbsUp />
+                Curtir <span>20</span>
+            </button>
+        </footer>
+        </div>
+    </div>
   )
 }
